9da33d7990ae704f3a5fead2887bad0847d2b3f5
a-t-h-i <aonlysmith@gmail.com>
2023-04-22 16:05:08 +0200

    Setting up skeleton code

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests, json
+
+def get_api_key():
+    return input("Please enter your OpenWeather API key:\n")
+
+
+def save_to_env(key):
+    pass
+
+
+def get_city_name():
+    return input("Enter name of city:\n")
+
+
+def is_correct_city(city):
+    pass
+
+
+def check_city_file(city):
+    pass
+
+
+def get_coordinates(city):
+    pass
+
+
+def get_current_weather(city):
+    pass
+
+
+def get_five_day_forcast(city):
+    pass
+
+
+def save_to_file(current_weather, five_day_weather):
+    pass
+
+
+def display_data():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
