3df87ce2171b6a409a48a576ee4c43e3be5080dc
a-t-h-i <aonlysmith@gmail.com>
2023-04-24 01:39:25 +0200

    Added docstrings

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from prettytable import PrettyTable
 
 
 def should_update(city):
+    """Checks if the file should be updated
+
+    Args:
+        city {str}: name of city
+
+    Returns:
+        {boolean}: true if the file should be updated and false if it should not.
+    """
     if has_city_file(city):
         for file in os.listdir("data"):
             if city in file:
@@ -16,19 +24,35 @@ def should_update(city):
 
 
 def has_env_file():
+    """Checks if .env file exists
+
+    Returns:
+        {boolean}: return true if file exists or false if not
+    """
     return exists(".env")
 
 
 def get_api_key():
+    """Basically gets the user input
+
+    Returns:
+        {str}: returns whatever the user has entered (Hopefully the api key ^_^)
+    """
     return input("Please enter your OpenWeather API key:\n")
 
 
 def save_to_env(key):
+    """Saves api key to .env file
+
+    Args:
+        key {str}: api key needed to make calls to openweather api
+    """
     with open(".env", "w") as f:
         f.write(f"KEY={key}")
 
 
 def set_env_variable():
+    """Sets the environment variable(s) for the api key using the values read from .env file"""
     with open(".env", "r") as f:
         for line in f.readlines():
             key, value = line.split("=")
@@ -36,6 +60,14 @@ def set_env_variable():
 
 
 def get_city_name(key):
+    """Gets the name of the city from user input and checks if the name doesn't return error 404
+
+    Args:
+        key {str}: api key needed for checking city name
+
+    Returns:
+        {str}: name of the city if no error was returned. (Also makes it uppercase just for consistency)
+    """
     while True:
         os.system("clear")
         city = input("Enter name of city:\n").upper()
@@ -51,15 +83,30 @@ def get_city_name(key):
 
 
 def has_city_file(city):
-    files = os.listdir("data")
+    """Checks if the file for the city exists
 
-    for file in files:
+    Args:
+        city {str}: name of city
+
+    Returns:
+        {boolean}: True if file exists and false if not
+    """
+    for file in os.listdir("data"):
         if city in file:
             return True
     return False
 
 
 def get_coordinates(city, key):
+    """Gets coordinates of given city
+
+    Args:
+        city {str}: name of city
+        key {str}: api key
+
+    Returns:
+        {tuple}: tuple with lon and lat of city
+    """
     result = requests.get(
         f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={key}"
     ).text
@@ -71,6 +118,15 @@ def get_coordinates(city, key):
 
 
 def get_current_weather(key, lat_lon):
+    """Gets the current weather on given location (lon, lat)
+
+    Args:
+        key {str}: api key
+        lat_lon {tuple}: tuple that has lat and 0 and lon at 1
+
+    Returns:
+        {dict}: map wich contains current weather
+    """
     result = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?lat={lat_lon[0]}&lon={lat_lon[1]}&appid={key}&units=metric"
     ).text
@@ -82,6 +138,15 @@ def get_current_weather(key, lat_lon):
 
 
 def get_3_hourly_forecast(lat_lon, key):
+    """Gets a forcast for every three hours from the current time
+
+    Args:
+        lat_lon {tuple}: tuple that has lat and 0 and lon at 1
+        key {str}: api key
+
+    Returns:
+        {tuple}: returns tuple with 3 items, the items are dicts with 3 hour interval weather data per item.
+    """
     result = requests.get(
         f"https://api.openweathermap.org/data/2.5/forecast?lat={lat_lon[0]}&lon={lat_lon[1]}&cnt=5&appid={key}&units=metric"
     ).text
@@ -110,7 +175,14 @@ def get_3_hourly_forecast(lat_lon, key):
 
 
 def save_to_file(city, lat_lon, data):
-    formatted_name = f"{city.upper()}_{lat_lon[0]}_{lat_lon[1]}.txt"
+    """Saves the prettified data to file
+
+    Args:
+        city {str}: name of city
+        lat_lon {tuple}: tuple that has lat and 0 and lon at 1
+        data {str}: weather data that has been formatted to appeal to the human eye.
+    """
+    formatted_name = f"{city}_{lat_lon[0]}_{lat_lon[1]}.txt"
 
     if has_city_file(city):
         for item in os.listdir("data"):
@@ -124,6 +196,16 @@ def save_to_file(city, lat_lon, data):
 
 
 def prettify_data(city, current_weather, three_hour_forecast):
+    """Puts weather data in a table so that it's easier to read
+
+    Args:
+        city {str}: name of city
+        current_weather {dict}: current weather at city
+        three_hour_forecast {tuple}: tuple with items reprisenting a 3 hour interval forecast for given city
+
+    Returns:
+        _type_: _description_
+    """
     table = PrettyTable()
 
     table.field_names = ["Time", "Temperature", "Description"]
@@ -156,9 +238,12 @@ def prettify_data(city, current_weather, three_hour_forecast):
 
 
 def display(city):
-    files = os.listdir("data")
+    """Reads weather from saved file and displays it for the user
 
-    for file in files:
+    Args:
+        city {str}: name of city
+    """
+    for file in os.listdir("data"):
         if city in file:
             with open(f"data/{file}", "r") as f:
                 for line in f.readlines():
@@ -167,8 +252,6 @@ def display(city):
 
 
 def main():
-    key, city = "", ""
-
     if not has_env_file():
         while True:
             key = get_api_key()
